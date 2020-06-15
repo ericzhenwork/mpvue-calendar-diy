1fefb1e7b4cd93dd0ee33316cbde97ee9ff87ead
张存福 <zhang5739@vip.qq.com>
2020-06-15 11:16:51 +0800

    init

---
+++ example/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Calendar />
+  </div>
+</template>
+
+<script>
+  import Calendar from './components/Calendar';
+
+  export default {
+    name: 'app',
+    components: {
+      Calendar
+    }
+  };
+</script>
+
+<style>
+  body {
+    background-color: #fbf9fe;
+  }
+</style>
